0834d5cebdf1382608b2694d05d796752db9c009
Nick <nlertola@live.com>
2023-01-27 00:15:39 -0700

    Added css and images

---
+++ js/localstorage.js
@@ -0,0 +1,13 @@
+﻿// Save data to localStorage
+export function setItem(key, val) {
+    localStorage.setItem(key, JSON.stringify(val));
+}
+// Get saved data from localStorage
+export function getItem(key) {
+    return localStorage.getItem(key) ? localStorage.getItem(key) : '0';
+}
+
+// Remove saved data from localStorage
+export function removeItem(key) {
+    localStorage.removeItem(key);
+}
